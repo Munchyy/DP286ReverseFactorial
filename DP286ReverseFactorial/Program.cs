7b97dcc594ff5b8c0496184ba49154e1e4e77983
Munchyy <max.pickup94@gmail.com>
2016-12-06 15:02:35 +0000

    Finished

--- DP286ReverseFactorial/Program.cs
+++ DP286ReverseFactorial/Program.cs
@@ -10,6 +10,23 @@ namespace DP286ReverseFactorial
     {
         static void Main(string[] args)
         {
+            string[] splitInputs = Properties.Resources.inputText.Split('\n');
+
+            int inputInt = 0;
+
+            foreach(string s in splitInputs)
+            {
+                if (int.TryParse(s,out inputInt))
+                {
+                    ReverseFactorial rFact = new ReverseFactorial(inputInt);
+                }
+                else
+                {
+                    Console.WriteLine("That's not an integer son!");
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ DP286ReverseFactorial/ReverseFactorial.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP286ReverseFactorial
+{
+    class ReverseFactorial
+    {
+        private int factorialInt;
+
+        public ReverseFactorial(int inputInt)
+        {
+            factorialInt = inputInt;
+            if (factorialInt < 0)
+            {
+                Console.WriteLine("This number is not positive");
+            }else
+            {
+                string output = FindReverseFactorial(factorialInt, 1).ToString();
+                if (output.Equals("-1"))
+                    output = "None";
+                Console.WriteLine("Base factorial of {0} is {1}!", factorialInt, output);
+            }
+        }
+
+
+        //recursive method to find the factorial for this number
+        private int FindReverseFactorial(int input, int divisor)
+        {
+            if (input == 1)
+                return 1;
+            else if (input % divisor == 0)
+            {
+
+                input = input / divisor;
+
+                if (input == 1)
+                    return divisor;
+                else
+                    return FindReverseFactorial(input, ++divisor);
+            }
+            else
+                return -1;
+        }
+    }
+}
